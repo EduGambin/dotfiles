64a5b3d068cc97289bbe69360e2555bb60ca12ef
Edu Gambín <gambinmonserratedu@gmail.com>
2024-02-22 01:02:48 +0100

    Alpha coloring now working

--- nvim/lua/plugin/alpha.lua
+++ nvim/lua/plugin/alpha.lua
@@ -12,9 +12,6 @@ return {
 			[[                                                                     ]],
 			[[                                                                     ]],
 			[[                                                                     ]],
-			[[                                                                     ]],
-			[[                                                                     ]],
-			[[                                                                   ]],
 			[[      ████ ██████           █████      ██                    ]],
 			[[     ███████████             █████                            ]],
 			[[     █████████ ███████████████████ ███   ███████████  ]],
@@ -22,30 +19,42 @@ return {
 			[[   █████████ ██████████ █████████ █████ █████ ████ █████  ]],
 			[[ ███████████ ███    ███ █████████ █████ █████ ████ █████ ]],
 			[[██████  █████████████████████ ████ █████ █████ ████ ██████]],
-			[[                                                                     ]],
 			[[─────────────────────────────────────────────────────────────────────]],
 			[[                                                                     ]],
 			[[   Absolutely worth the 42-hour epic saga to configure this editor   ]],
-			[[                                                                     ]],
-			[[                                                                     ]],
 		}
 
 		-- Set menu
 		dashboard.section.buttons.val = {
-			dashboard.button("n", "󰈔  > New file",     "<cmd> ene <BAR> startinsert <cr>"),
-			dashboard.button("f", "  > Find file",    "<cmd> Telescope find_files  <cr>"),
-			dashboard.button("g", "  > Find text",    "<cmd> Telescope live_grep   <cr>"),
+			dashboard.button("n", "󰈔  > New file", "<cmd> ene <BAR> startinsert <cr>"),
+			dashboard.button("f", "  > Find file", "<cmd> Telescope find_files  <cr>"),
+			dashboard.button("g", "  > Find text", "<cmd> Telescope live_grep   <cr>"),
 			dashboard.button("r", "  > Find recent ", "<cmd> Telescope oldfiles    <cr>"),
 			dashboard.button("l", "󰒲  > Open plugins", "<cmd> Lazy                  <cr>"),
-			dashboard.button("q", "  > Quit NeoVim",  "<cmd> qa                    <cr>"),
+			dashboard.button("q", "  > Quit NeoVim", "<cmd> qa                    <cr>"),
 		}
 
 		-- Configure highlighting
 		for _, button in ipairs(dashboard.section.buttons.val) do
-			button.opts.hl = { { "Define", 0, 2 }, { "Constant", 3, 7 } }
-			button.opts.hl_shortcut = "Special"
+			button.opts.hl = { { "Define", 0, 4 }, { "Boolean", 5, 7 } }
+			button.opts.hl_shortcut = "Boolean"
 		end
-		dashboard.section.header.opts.hl = "Identifier"
+		dashboard.section.header.opts.hl = {
+			{ {"", 0, -1} },
+			{ {"", 0, -1} },
+			{ {"", 0, -1} },
+			{ {"", 0, -1} },
+			{ { "Function", 0, 50 },  { "String", 51, -1 }, },
+			{ { "Function", 0, 50 },  { "String", 51, -1 }, },
+			{ { "Function", 0, 89 },  { "String", 90, -1 }, },
+			{ { "Function", 0, 84 },  { "String", 85, -1 }, },
+			{ { "Function", 0, 96 },  { "String", 97, -1 }, },
+			{ { "Function", 0, 96 },  { "String", 97, -1 }, },
+			{ { "Function", 0, 106 }, { "String", 107, -1 }, },
+			{ {"Comment", 0, -1} },
+			{ {"", 0, -1} },
+			{ {"Comment", 0, -1} },
+		}
 
 		-- Send config to alpha
 		alpha.setup(dashboard.opts)
